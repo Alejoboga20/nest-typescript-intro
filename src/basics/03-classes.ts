f6467f663f26c506a24bd54954e4269edd04586f
Alejo Boga <alejoboga20@gmail.com>
2023-08-24 14:53:59 -0300

    getters and methods

--- src/basics/03-classes.ts
+++ src/basics/03-classes.ts
@@ -14,6 +14,18 @@ export class Pokemon {
 
 /* Short form for classes */
 export class Pokemon {
+	get imageUrl(): string {
+		return `https://pokemon.com/${this.id}`;
+	}
+
+	scream(): void {
+		console.log(`${this.name.toUpperCase()}!!!}`);
+	}
+
+	speak(): void {
+		console.log(`Hi my name is ${this.name}`);
+	}
+
 	constructor(public readonly id: number, public name: string) {}
 }
 
